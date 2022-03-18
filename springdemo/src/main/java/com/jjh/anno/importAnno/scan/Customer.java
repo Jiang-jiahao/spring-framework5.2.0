38f66c1b0d28748a7e382958130e7faa8a903869
jiangjiahao <Wohenwuqu123>
2022-03-18 16:18:35 +0800

    添加@Import的ImportBeanDefinitionRegistrar相关使用

--- springdemo/src/main/java/com/jjh/anno/importAnno/scan/Customer.java
+++ springdemo/src/main/java/com/jjh/anno/importAnno/scan/Customer.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
 //@Import(ConfigurationA.class)
-@Import(ConfigurationB.class)
+@Import({ConfigurationB.class,ConfigurationC.class})
 @Component
 public class Customer {
 }
